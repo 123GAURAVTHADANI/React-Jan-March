125f52facb180c065c5f00167c5996f54d62fbab
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-09-14 13:29:45 +0530

    Jan & March

---
+++ src/Components/Febr.js
@@ -0,0 +1,4 @@
+function Febr() {
+  return <h2>Feb</h2>;
+}
+export default Febr;
---
+++ src/Components/January.js
@@ -0,0 +1,12 @@
+import Febr from "./Febr";
+
+function January() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Febr />
+    </div>
+  );
+}
+
+export default January;
